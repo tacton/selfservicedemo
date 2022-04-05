f890a1882f466ddb002ef27baf888e73db18b694
Krzysztof.Falatowicz <krzysztof.falatowicz@tacton.com>
2022-04-05 11:47:01 +0200

    CSRF token

--- src/main/java/com/tacton/config/SecurityConfig.java
+++ src/main/java/com/tacton/config/SecurityConfig.java
@@ -108,7 +108,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .cors().disable()
                 .authorizeRequests()
                 .antMatchers(AdminMatchers).hasRole("ADMIN")
